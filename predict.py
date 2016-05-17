f4a61c333bfb2417078490e1baab5d5871917329
Michael Maduabum <michaelmaduabum@DN51usjv.SUNet>
2016-05-17 16:38:20 -0700

    got basic SVM structure working with garbage features and fake classes

--- predict.py
+++ predict.py
@@ -2,6 +2,7 @@
 import utils
 import sys
 import random
+import time
 import numpy as np
 from sklearn import cross_validation
 from sklearn import svm
@@ -10,15 +11,17 @@ from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_sc
 
 METHOD_NN = 0
 METHOD_SVM = 1
+BASELINE = 2
 BASELINE_VECTOR_SIZE = 20
 
 
 #Select option: SVM or NN
 def get_method():
 	print "Which method would you like to test?"
-	method = raw_input("(N)eural nets or (S)VM:")
+	method = raw_input("(N)eural nets or (S)VM or simple (B)aseline:")
 	if method[0] == 'N': return METHOD_NN
 	elif method[0] == 'S': return METHOD_SVM
+	elif method[0] == 'B': return BASELINE
 	else:
 		print "Invalid option"
 		return get_method()
@@ -33,10 +36,19 @@ def get_data():
 #train the 5  1 vs all classifiers
 #train the 10 binary classifiers
 def train_svm():
+	#data in (text, rating) pairs
 	raw_data = get_data()
-	input_data = np.array([random.uniform(-0.5, 0.5) for i in range(BASELINE_VECTOR_SIZE)]) 
-	target_data = [int(pair[1]) for pair in raw_data]
-	X_train. X_test, Y_train, Y_test = cross_validation.train_test_split(input_data, target_data, test_size=0.1, random_state=0)
+	#create feature vectors for each review text. Currently just using a random vector
+	input_data = np.array([np.array([random.uniform(-0.5, 0.5) for i in range(BASELINE_VECTOR_SIZE)]) for j in range(len(raw_data))]) 
+	#based on the classifier being trained, generate a list of labels for each train example
+	#e.g. if training 1 vs all labels can be 1, -1
+	target_data = [int(pair[1]) % 2 for pair in raw_data]
+	state = random.randint(0, int(time.time()))
+	#Once data has been translated to feature vector and target classes have been decided, train the model
+	#Plan is to keep 15 of these clf's so that any unseen review can be classified
+	X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(input_data, target_data, test_size=0.1, random_state=state)
+	clf = svm.SVC(kernel='linear', C=1).fit(X_train, Y_train)
+	print clf.score(X_test, Y_test) 
 
 def train_nn():
 	pass
@@ -69,11 +81,13 @@ def report_results(y_test, y_pred):
 
 
 def main():
-	#method = get_method();
-	#if method == 0: train_nn()
-	#else: train_svm()
-
-	run_baseline()
+	#set a random seed
+	random.seed(int(time.time()))
+	#get which prediction method to use
+	method = get_method();
+	if method == METHOD_NN: train_nn()
+	elif method == METHOD_SVM: train_svm()
+	else: run_baseline()
 
 
 
