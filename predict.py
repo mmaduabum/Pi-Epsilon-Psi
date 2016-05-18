0518d2871b5d767eeb6b718d8b90e048414add80
Michael Maduabum <michaelmaduabum@DN51usjv.SUNet>
2016-05-17 17:01:02 -0700

    changed some file names and restructured

--- predict.py
+++ predict.py
@@ -3,6 +3,8 @@ import utils
 import sys
 import random
 import time
+import our_svm
+import our_nn
 import numpy as np
 from sklearn import cross_validation
 from sklearn import svm
@@ -33,24 +35,12 @@ def get_data():
 	raw_data = utils.create_train_data('yelp_data/tiny.json')
 	return raw_data
 
-#train the 5  1 vs all classifiers
-#train the 10 binary classifiers
-def train_svm():
-	#data in (text, rating) pairs
-	raw_data = get_data()
-	#create feature vectors for each review text. Currently just using a random vector
-	input_data = np.array([np.array([random.uniform(-0.5, 0.5) for i in range(BASELINE_VECTOR_SIZE)]) for j in range(len(raw_data))]) 
-	#based on the classifier being trained, generate a list of labels for each train example
-	#e.g. if training 1 vs all labels can be 1, -1
-	target_data = [int(pair[1]) % 2 for pair in raw_data]
-	state = random.randint(0, int(time.time()))
-	#Once data has been translated to feature vector and target classes have been decided, train the model
-	#Plan is to keep 15 of these clf's so that any unseen review can be classified
-	X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(input_data, target_data, test_size=0.1, random_state=state)
-	clf = svm.SVC(kernel='linear', C=1).fit(X_train, Y_train)
-	print clf.score(X_test, Y_test) 
+def run_svm_classifier():
+	data = get_data()
+	our_svm.train_svms(data)
 
-def train_nn():
+
+def run_nn_classifier():
 	pass
 
 #to train:
@@ -72,7 +62,8 @@ def run_random_baseline(k=5):
 	predictions = [random.randint(1, 5) for _ in raw_data]
 	report_results(target_data, predictions)
 
-def run_baseline():
+
+def run_svm_baseline():
 	raw_data = get_data()
 	input_data = np.array([np.array([random.uniform(-0.5, 0.5) for i in range(BASELINE_VECTOR_SIZE)]) for j in range(len(raw_data))])
 	target_data = [int(pair[1]) for pair in raw_data]
@@ -80,6 +71,11 @@ def run_baseline():
 	X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(input_data, target_data, test_size=0.1, random_state=state)
 	clf = svm.SVC(kernel='linear', C=1).fit(X_train, Y_train)
 	print clf.score(X_test, Y_test) 
+
+
+def run_baselines():
+	run_svm_baseline();
+	#run_nn_baseline()
 
 def report_results(y_test, y_pred):
 	print(f1_score(y_test, y_pred, average="macro"))
@@ -93,9 +89,9 @@ def main():
 	random.seed(int(time.time()))
 	#get which prediction method to use
 	method = get_method();
-	if method == METHOD_NN: train_nn()
-	elif method == METHOD_SVM: train_svm()
-	else: run_baseline()
+	if method == METHOD_NN: run_nn_classifier()
+	elif method == METHOD_SVM: run_svm_classifier()
+	else: run_baselines()
 
 
 
